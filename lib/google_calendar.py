34c98631e635e65530d335ddf15de5e67047acd6
Caleb Dickson <157729906+CalebDksn25@users.noreply.github.com>
2026-01-27 11:51:54 -0800

    Added create task function

--- lib/google_calendar.py
+++ lib/google_calendar.py
@@ -1,5 +1,8 @@
 from googleapiclient.discovery import build
 
+"""
+Function to create an event in the google calendar from the item dictionary.
+"""
 def create_calendar_event(creds, item: dict, calendar_id: str = "primary"):
 
     # Build the Google Calendar service
@@ -19,3 +22,28 @@ def create_calendar_event(creds, item: dict, calendar_id: str = "primary"):
 
     # Return the link to the created event
     return created.get('htmlLink')
+
+
+"""
+Function to create a google task from the item dictionary.
+"""
+def create_task(creds, item: dict, tasklist_id: str = "@default") -> str:
+
+    # Build the Google Tasks service
+    service = build("tasks", "v1", credentials=creds)
+
+    # Create the task body based on item dictionary
+    task = {
+        "title": item["title"].strip().title(),
+        "notes": item.get("notes") or "",
+    }
+
+    # Google tasks "due" expects RFC3339 Date formate. V1: end-of-day UTC
+    if item.get("due_date"):
+        task["due"] = f"{item['due_date']}T23:59:00Z"
+
+    # Create the task in the list
+    created = service.tasks().insert(tasklist=tasklist_id, body=task).execute()
+
+    # Return the task ID
+    return created.get('id')

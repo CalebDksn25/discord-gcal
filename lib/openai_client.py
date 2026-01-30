890907a603177d91e82bedafd649a7ae9de8874d
Caleb Dickson <157729906+CalebDksn25@users.noreply.github.com>
2026-01-29 17:48:55 -0800

    openai date function

--- lib/openai_client.py
+++ lib/openai_client.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 from openai import AsyncOpenAI
 import os
-from lib.prompts import get_user_prompt, OPENAI_SYSTEM_PROMPT
+from lib.prompts import get_user_prompt, OPENAI_SYSTEM_PROMPT, OPENAI_DATE_PROMPT
 
 
 # Load environment variables from .env file
@@ -21,6 +21,24 @@ async def get_openai_response(user_input: str):
         messages=[
             {"role": "system", "content": OPENAI_SYSTEM_PROMPT},
             {"role": "user", "content": user_prompt}
+        ],
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+
+    # Return the content of the response
+    return response.choices[0].message.content
+
+# Create a function to get a response for date specific prompt via OpenAI
+async def get_date_response(user_input: str):
+    # Call the OpenAI API asynchronously
+    response = await client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": OPENAI_DATE_PROMPT},
+            {"role": "user", "content": user_input}
         ],
         max_tokens=150,
         n=1,
